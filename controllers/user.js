7a048808de07475ef532ab2cd10cd8f8aedbc195
StephenReiser <Stephen.Reiser1@gmail.com>
2019-04-30 13:45:25 -0400

    added in some logic to not allow duplicate userid

--- controllers/user.js
+++ controllers/user.js
@@ -3,11 +3,11 @@ const users = express.Router()
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 
-users.get('/new/error', (request, response) => {
-    // response.render('user/newerror.ejs', {
-    //     currentUser: request.session.currentUser
-    // })
-    response.send('Error')
+users.get('/error', (request, response) => {
+    response.render('user/newerror.ejs', {
+        currentUser: request.session.currentUser
+    })
+    // response.send('Error')
 
     //not sure why this doesn't work, but feels liek it should
     
@@ -24,13 +24,15 @@ users.get('/new', (request, response) => {
 users.post('/', (request, response) => {
     request.body.password = bcrypt.hashSync(request.body.password, bcrypt.genSaltSync(10))
     User.create(request.body, (error, createdUser)=> {
-        console.log(createdUser)
+        // console.log(createdUser)
         if(error) {
-            response.redirect('/users/new/error', {
-                // this is erroring out because it isn't getting an error - createduser is returning something weird
-            })
-        } else if(createdUser === undefined) {response.redirect('/users/new/error')
-    } else {
+            // response.redirect('/users/new/error', {
+            //     // this is erroring out because it isn't getting an error - createduser is returning something weird
+            // })
+            console.log(error)
+            response.redirect('/users/error')
+        } 
+    else {
         request.session.currentUser = createdUser.username
             response.redirect('/map')
     }})
